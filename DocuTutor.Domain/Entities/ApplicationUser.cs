306a2810a43baf2c480be63214749343255525af
MariamMagdy <mariammagdyhasehim@gmail.com>
2026-06-27 04:34:19 +0300

    (feat):add IdentityappUser

--- DocuTutor.Domain/Entities/ApplicationUser.cs
+++ DocuTutor.Domain/Entities/ApplicationUser.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DocuTutor.Domain.Entities
 {
-    public class ApplicationUser
+    public class ApplicationUser :IdentityUser
     {
+        public string? FullName { get; private set; }
+
+        public bool IsActive { get; private set; }
+
+        public DateTime? LastLoginAt { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime UpdatedAt { get; private set; }
+        public virtual ICollection<RefreshToken>? RefreshTokens { get; set; } = new List<RefreshToken>();
+
+    
     }
 }
